f6e7b34c8c7d50baadcaf07bbb029760ee91e643
dexter73710 <collabmanaging@gmail.com>
2025-07-05 02:58:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const PORT = 3000;
 
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>🐥 Big Bird from Node.js!</h1>');
-}).listen(PORT, () => {
-  console.log(`Node app listening at http://localhost:${PORT}`);
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+      res.end('Error loading file');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+      res.end(data);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
